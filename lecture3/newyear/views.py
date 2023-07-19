01b18fba05635c73b4f2f018a20a39941d58b51f
IVINE SHAJI <ivineshaji123@gmail.com>
2023-07-19 16:52:07 +0400

    bug fixed

--- lecture3/newyear/views.py
+++ lecture3/newyear/views.py
@@ -7,5 +7,5 @@ from django.shortcuts import render
 def index(request) :
     now = datetime.datetime.now()
     return render(request, "newyear/index.html", { 
-        "newyear" : now.month == 12 and now.day == 25
+        "newyear" : now.month == 1 and now.day == 1
         })
