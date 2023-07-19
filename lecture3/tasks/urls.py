e4a0d454887aa7b9ec6636c90e791be30525518f
IVINE SHAJI <ivineshaji123@gmail.com>
2023-07-19 19:46:05 +0400

    URL updated along with adding the app_name

--- lecture3/tasks/urls.py
+++ lecture3/tasks/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "tasks" # Unique name for app
+
 urlpatterns = [
     path("" , views.index, name = "index"),
     path("add", views.add,name = "add")
