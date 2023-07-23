07e66e6e64cae8726226cfae5cc0177f9edeba10
IVINE SHAJI <ivineshaji123@gmail.com>
2023-07-23 13:16:02 +0400

    form is updated by using django forms

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,10 +6,16 @@ from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
 
+class NewTaskForm(forms.Form) :
+    task = forms.CharField(label = "New Task")
+    priority = forms.IntegerField(label = "Priority", min_value= 1, max_value= 99)
+
 def index(request) :
     return render(request ,"tasks/index.html" , {
         "tasks" : tasks 
     })
 
 def add(request) :
-    return render(request , "tasks/add.html")
+    return render(request , "tasks/add.html", {
+        "form" : NewTaskForm()
+    })
